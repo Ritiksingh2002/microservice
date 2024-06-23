bb7ac34a41f0bf4fba92c0e6479b0627c455ae0b
Ritik Singh <singhritik955@gmail.com>
2024-06-23 22:18:12 +0530

    service registry

---
+++ employee-service/employee-service/src/main/java/com/employee_service/employee_service/dto/APIResponseDto.java
@@ -0,0 +1,15 @@
+package com.employee_service.employee_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponseDto {
+    private EmployeeDto employee;
+    private DepartmentDto department;
+}
